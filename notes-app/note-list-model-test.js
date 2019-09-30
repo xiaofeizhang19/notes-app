c66a03c4383326bf9badc4e256600b06bdeb5d63
Xiaofei Zhang <xiaofeizhang@Xiaofeis-MBP.lan>
2019-10-01 00:09:53 +0100

    Add function for adding new note

--- notes-app/note-list-model-test.js
+++ notes-app/note-list-model-test.js
@@ -1,20 +1,18 @@
 (function(exports) {
   function testNoteListModel() {
-  var noteList = new NoteList();
+    var noteList = new NoteList();
 
-  if (noteList.allNotes.length !== 0 || noteList.allNotes === undefined){
-    throw new Error("List is not an empty array")
-  }
+    if (noteList.allNotes().length !== 0 || noteList.allNotes() === undefined){
+      throw new Error("List is not an empty array")
+    }
 
-  noteList.addNew("hello world");
-  console.log(noteList.allNotes.length);
-  console.log(noteList.allNotes[0]);
-  if (noteList.allNotes.length !== 1) {
-    throw new Error("List does not have the new note")
-  }
-  if (noteList.allNotes[0] !== "hello world") {
-    throw new Error("List does not include the new note")
-  }
+    noteList.addNew("hello world");
+    if (noteList.allNotes().length !== 1) {
+      throw new Error("List does not have one note")
+    }
+    if (noteList.allNotes()[0] !== "hello world") {
+      throw new Error("List does not include the new note")
+    }
 };
 
 testNoteListModel();
